87b747268dd7ec76ac52a8c286539cf0d0bd0e67
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2021-12-05 18:08:47 +0900

    add packet classes

--- manta_client/base/packet.py
+++ manta_client/base/packet.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Optional, Union
 
 """
-Packet & Result: Can be saved for offline or user wants
-Request & Response: 
+
+Packet: Can be saved for offline or user wants
+Request & Response: interaction with server 
 """
 
 
@@ -11,6 +12,21 @@ Request & Response:
 class Packet:
     key: str
     value: Any
+    req_result: int = 0  # 0 for false, 1 for true
+    result: Optional["Response"] = None
+
+    # Like heartbeats
+    history: Optional["HistoryPacket"] = None
+    summary: Optional["SummaryPacket"] = None
+    console: Optional["ConsolePacket"] = None
+    stats: Optional["StatsPacket"] = None
+    # Less frequent
+    artifact: Optional["ArtifactPacket"] = None
+    alarm: Optional["AlarmPacket"] = None
+    experiment: Optional["ExperimentPacket"] = None
+    settings: Optional["SettingsPacket"] = None
+    config: Optional["ConfigPacket"] = None
+    meta: Optional["MetaPacket"] = None
 
     @classmethod
     def init_from(cls, obj):
@@ -21,33 +37,97 @@ class Packet:
 
 
 @dataclass
-class HistoryItem:
+class _HistoryItem:
     key: str
     value: Union[str, int, float]
 
 
 @dataclass
 class HistoryPacket:
-    item: HistoryItem
+    item: List[_HistoryItem]
     step: int
 
 
 @dataclass
-class StatsItem:
+class SummaryPacket:
+    pass
+
+
+@dataclass
+class ConsolePacket:
+    stream: str  # stderr or stdout
+    timestamp: int
+    lines: str
+
+
+@dataclass
+class _StatsItem:
     key: str
     value: Dict
 
 
 @dataclass
 class StatsPacket:
-    item: StatsItem
+    item: List[_StatsItem]
     timestamp: int
     step: int
 
 
 @dataclass
-class Result:
+class ArtifactPacket:
+    experiment_id: str
+    experiment_name: str
+    project: str
+    entity: str
+    checksum: str
+    description: str
+    contents: List["ArtifactManifest"]
+
+
+@dataclass
+class ArtifactManifest:
+    version: int
+    zipped: bool
+    storage: str
+    detail: "ArtifactManifestDetail"
+
+
+@dataclass
+class ArtifactManifestDetail:
+    path: str
+    checksum: str
+    size: int
+    local_path: str
+
+
+@dataclass
+class AlarmPacket:
+    title: str
+    text: str
+    level: str
+
+
+@dataclass
+class ExperimentPacket:
+    experiment_id: str
+    entity: str
+    project: str
+
+
+@dataclass
+class SettingsPacket:
     pass
+
+
+@dataclass
+class ConfigPacket:
+    pass
+
+
+@dataclass
+class MetaPacket:
+    group: str
+    job: str
 
 
 @dataclass
@@ -57,4 +137,34 @@ class Request:
 
 @dataclass
 class Response:
+    pass
+
+
+@dataclass
+class LoginRequest:
+    pass
+
+
+@dataclass
+class LoginResponse:
+    pass
+
+
+@dataclass
+class ExperimentStartRequest:
+    pass
+
+
+@dataclass
+class ExperimentStartResponse:
+    pass
+
+
+@dataclass
+class ArtifactRequest:
+    pass
+
+
+@dataclass
+class ArtifactResponse:
     pass
